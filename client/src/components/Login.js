4d06b25848812cc07fe709a481cfe7bae1aeaa31
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2019-12-20 08:42:18 -0800

    added private routes, authorization with axios, and changed the username and password on the server

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { authAxios } from "../utils/authAxios";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({username: '', password: ''});
+  const [isFetching, setIsFetching] = useState(false);
+
+  const handleLogin = event => {
+  	event.preventDefault();
+  	setIsFetching(true);
+  	authAxios().post("/login", credentials)
+  			   .then(res => {
+  			   		setIsFetching(true);
+  			   		console.log(res)
+  			   })
+  			   .catch(err => {
+  			   	console.log(err)
+  			   	alert("an error occured");
+  			   	setIsFetching(false);
+  			   });
+  }
+
+  const handleChanges = event => {
+  	setCredentials({ ...credentials, [event.target.name]: event.target.value})
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      {isFetching ? <p>...fetching...</p> : <></>}
+      <form onSubmit={handleLogin}>
+      	<input
+      		type="text"
+      		name="username"
+      		onChange={handleChanges}
+      		value={credentials.username}
+      	/>
+      	<input
+      		type="text"
+      		name="password"
+      		onChange={handleChanges}
+      		value={credentials.password}
+      	/>
+      	<button>Login</button>
+      </form>
     </>
   );
 };
